4e1cbd5047de3728a2a9b5acf6cb34f3f51f458a
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2020-04-16 14:44:59 +0900

    [Fix] 一時変数 totalAmount に「ステートメントのスライド」を適用

--- chapter01/statement.ts
+++ chapter01/statement.ts
@@ -1,11 +1,11 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
     // 注文の内訳を出力
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
+  let totalAmount = 0;
   for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
