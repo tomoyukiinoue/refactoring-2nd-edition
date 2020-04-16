549c8904f7de3454e004a6e862d8cddec9513d2e
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2020-04-16 18:04:47 +0900

    [Fix] volumeCreditsForに「関数のインライン化」を適用

--- chapter01/createStatementData.ts
+++ chapter01/createStatementData.ts
@@ -12,16 +12,12 @@ export function createStatementData(invoice, plays) {
     const result: any = { ...aPerformance };
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
   }
 
   function totalAmount(data) {
