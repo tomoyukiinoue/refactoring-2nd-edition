3c6faa77915910fde9aac5088a7fe604e1198c27
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2020-04-16 16:41:16 +0900

    [Fix]  計算とフォーマットにフェーズを分離 (#3)
    * [Fix] Plain Text 出力部分に「関数の抽出」を適用
    
    * [Fix] renderPlainText に「変数宣言の変更」を適用: invoice 引数削除
    
    * [Fix] invoice.performances をシャローコピー
    
    * [Fix] playFor 関数に「関数の移動」を適用し、statement関数側に移動
    
    * [Fix] amountFor 関数に「関数の移動」を適用し、statement 関数側に移動
    
    * [Fix] volumeCreditsFor 関数に「関数の移動」を適用し、statement 関数側に移動
    
    * [Fix] 合計の計算を statement 関数側に移動
    
    * [Fix] data の生成方法を修正
    
    * [Fix] 請求額、ボリューム特典ポイントの合計計算に「パイプラインによるループの置き換え」を適用
    
    * statementData の生成部分に「関数の抽出を適用」
    
    * [Fix] createStatementData 関数内の変数に「変数名の変更」を適用
    
    * [Fix] createStatementData 関数に「関数宣言の変更」を適用
    
    * [Fix] 中間データ生成部分のファイルを分割
    
    * [Fix] usd関数を他のrender関数から使えるように「関数の移動」を適用

--- chapter01/statement.ts
+++ chapter01/statement.ts
@@ -1,68 +1,24 @@
+import { createStatementData } from './createStatementData';
+
 export function statement(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+  const statementData: any = createStatementData(invoice, plays);
+  return renderPlainText(statementData, plays);
+}
 
-  for (let perf of invoice.performances) {
+function usd(aNumber) {
+  return new Intl.NumberFormat('en-US', {
+  style: 'currency', currency: 'USD',
+    minimumFractionDigits: 2
+  }).format(aNumber / 100);
+}
+
+function renderPlainText(data: any, plays: any) {
+  let result = `Statement for ${data.customer}\n`;
+  for (let perf of data.performances) {
     // 注文の内訳を出力
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
-
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += amountFor(perf);
-    }
-    return result;
-  }
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += volumeCreditsFor(perf);
-    }
-    return result;
-  }
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat('en-US',
-                        { style: 'currency', currency: 'USD',
-                          minimumFractionDigits: 2 }).format(aNumber/100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    // 喜劇のときは10人につき、さらにポイントを加算
-    if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-    case 'tragedy':
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case 'comedy':
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-    return result;
-  }
 }
