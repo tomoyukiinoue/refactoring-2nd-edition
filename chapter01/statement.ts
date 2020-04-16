f84121b8aea5156389dee865d6c72779d46a4858
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2020-04-16 14:54:58 +0900

    [Fix] statement 関数の分割 (#2)
    * [Fix] switch ブロックに「関数の抽出」を適用
    
    * [Fix] 変数の名称変更 thisAmount → result
    
    * [Fix] 引数の名称変更 perf → aPerformance
    
    * [Fix] play変数に「問い合わせによる一時変数の置き換え」を適用
    
    * play変数に「変数のインライン化」を適用
    
    * [Fix] amountFor 関数に「関数宣言の変更」を適用 STEP1
    
    * [Fix] amountFor 関数に「関数宣言の変更」を適用 STEP2 (パラメータ削除)
    
    * thisAmount 変数に「変数のインライン化」を適用
    
    * [Fix] ボリューム特典のポイントの計算部分に「関数の抽出」を適用
    
    * [Fix] volumeCreditsFor 関数 「変数名の変更」volumeCredits -> result
    
    * [Fix] volumeCreditsFor 関数 「変数名の変更」perf -> aPerformance
    
    * [Fix] 一時変数 format に「関数の抽出」を適用
    
    * [Fix] format関数に「関数宣言の変更」を適用 format -> usd
    
    * [Fix] 100で割る処理をフォーマット用の関数に移動
    
    * [Fix] 「ループの分離」を適用
    
    * [Fix] 一時変数 volumeCredits に「ステートメントのスライド」を適用
    
    * [Fix] volumeCredits の集計箇所に「関数の抽出」を適用
    
    * [Fix] 一時変数 volumeCredits に「変数のインライン化」を適用
    
    * [Fix] 合計計算箇所に「ループの分離」を適用
    
    * [Fix] 一時変数 totalAmount に「ステートメントのスライド」を適用
    
    * [Fix] 合計計算箇所に「関数の抽出」を適用（関数名は一時変数と重複するため仮名）
    
    * [Fix] 合計の一次変数に「変数のインライン化」を適用
    
    * [Fix] 合計計算の関数に「関数宣言の変更」を適用し、仮名から正式名に変更
    
    * [Fix] 関数内の変数に「変数名の変更」を適用

--- chapter01/statement.ts
+++ chapter01/statement.ts
@@ -1,43 +1,68 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
-  const format = new Intl.NumberFormat('en-US',
-                        { style: 'currency', currency: 'USD',
-                          minimumFractionDigits: 2 }).format;
-
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
+    // 注文の内訳を出力
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+  }
 
-    switch (play.type) {
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
+  return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US',
+                        { style: 'currency', currency: 'USD',
+                          minimumFractionDigits: 2 }).format(aNumber/100);
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    // 喜劇のときは10人につき、さらにポイントを加算
+    if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
     case 'tragedy':
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case 'comedy':
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
-      throw new Error(`unknown type: ${play.type}`);
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
-
-    // ボリューム特典のポイントを追加
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 喜劇のときは10人につき、さらにポイントを加算
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    // 注文の内訳を出力
-    result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    return result;
   }
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
 }
