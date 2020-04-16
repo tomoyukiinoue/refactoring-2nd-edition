038806a9b56c4da862258661e019283fd5a510e5
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2020-04-16 18:27:37 +0900

    [Fix] TragedyCalculator クラス、ComedyCalculator クラスを作成

--- chapter01/createStatementData.ts
+++ chapter01/createStatementData.ts
@@ -17,7 +17,12 @@ export function createStatementData(invoice, plays) {
   }
 
   function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch(aPlay.type) {
+      case 'tragedy': return new TragedyCalculator(aPerformance, aPlay);
+      case 'comedy': return new ComedyCalculator(aPerformance, aPlay);
+      default:
+        throw new Error(`未知の演劇の種類: ${aPlay.type}`);
+    }
   }
 
   function playFor(aPerformance) {
@@ -74,4 +79,10 @@ class PerformanceCalculator {
       result += Math.floor(this.performance.audience / 5);
     return result;
   }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+}
+
+class ComedyCalculator extends PerformanceCalculator {
 }
