62928c7e40bacf3828b69952ac5b23e8c0555077
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2020-04-16 18:01:33 +0900

    [Fix] amountFor に「関数のインライン化」を適用

--- chapter01/createStatementData.ts
+++ chapter01/createStatementData.ts
@@ -11,17 +11,13 @@ export function createStatementData(invoice, plays) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result: any = { ...aPerformance };
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
 
   function volumeCreditsFor(aPerformance) {
