74062050879f3d2a4f54c0ba13c3b2ab3554329b
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2020-04-16 15:47:19 +0900

    [Fix] playFor 関数に「関数の移動」を適用し、statement関数側に移動

--- chapter01/statement.ts
+++ chapter01/statement.ts
@@ -4,18 +4,25 @@ export function statement(invoice, plays) {
     performances: invoice.performances.map(enrichPerformance),
   };
   return renderPlainText(statementData, plays);
-}
 
-function enrichPerformance(aPerformance) {
-  const result = { ...aPerformance };
-  return result;
+  function enrichPerformance(aPerformance) {
+    const result = {
+      ...aPerformance,
+      play: playFor(aPerformance),
+    };
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 }
 
 function renderPlainText(data: any, plays: any) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
     // 注文の内訳を出力
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -45,16 +52,13 @@ function renderPlainText(data: any, plays: any) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
     // 喜劇のときは10人につき、さらにポイントを加算
-    if ('comedy' === playFor(aPerformance).type)
+    if ('comedy' === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case 'tragedy':
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -69,9 +73,8 @@ function renderPlainText(data: any, plays: any) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+        throw new Error(`unknown type: ${aPerformance.play.type}`);
     }
     return result;
   }
 }
-
