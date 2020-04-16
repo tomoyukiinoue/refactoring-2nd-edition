60e4ed16cefd07bba849097db74cbd23062f62bf
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2020-04-16 14:38:28 +0900

    [Fix] volumeCredits の集計箇所に「関数の抽出」を適用

--- chapter01/statement.ts
+++ chapter01/statement.ts
@@ -7,14 +7,19 @@ export function statement(invoice, plays) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  let volumeCredits = totalVolumeCredits();
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US',
