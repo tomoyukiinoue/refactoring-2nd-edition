fcf467e5401fcedc154fcb00f9b65b131b7ea8dc
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2020-04-16 18:03:48 +0900

    [Fix] PerformanceCalculator に volumeCreditsFor を「関数の移動」

--- chapter01/createStatementData.ts
+++ chapter01/createStatementData.ts
@@ -21,12 +21,7 @@ export function createStatementData(invoice, plays) {
   }
 
   function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    // 喜劇のときは10人につき、さらにポイントを加算
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
   }
 
   function totalAmount(data) {
@@ -68,6 +63,15 @@ class PerformanceCalculator {
       default:
         throw new Error(`unknown type: ${this.play.type}`);
     }
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    // 喜劇のときは10人につき、さらにポイントを加算
+    if ('comedy' === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
     return result;
   }
 }
