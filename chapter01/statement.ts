8fe4e2fd8cef9eb50449368f2896f6aa7cf24b55
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2020-04-16 16:10:54 +0900

    [Fix] volumeCreditsFor 関数に「関数の移動」を適用し、statement 関数側に移動

--- chapter01/statement.ts
+++ chapter01/statement.ts
@@ -13,6 +13,7 @@ export function statement(invoice, plays) {
     result = {
       ...result,
       amount: amountFor(result),
+      volumeCredits: volumeCreditsFor(result),
     };
     return result;
   }
@@ -42,6 +43,15 @@ export function statement(invoice, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    // 喜劇のときは10人につき、さらにポイントを加算
+    if ('comedy' === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 function renderPlainText(data: any, plays: any) {
@@ -64,7 +74,7 @@ function renderPlainText(data: any, plays: any) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -74,13 +84,4 @@ function renderPlainText(data: any, plays: any) {
       minimumFractionDigits: 2
     }).format(aNumber / 100);
   }
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    // 喜劇のときは10人につき、さらにポイントを加算
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
 }
