f34d31013c41d21294e195c42457c84ac442dfe4
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2020-04-16 15:29:30 +0900

    [Fix] Plain Text 出力部分に「関数の抽出」を適用

--- chapter01/statement.ts
+++ chapter01/statement.ts
@@ -1,11 +1,14 @@
 export function statement(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+  const statementData = {};
+  return renderPlainText(statementData, invoice, plays);
+}
 
+function renderPlainText(data: any, invoice: any, plays: any) {
+  let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     // 注文の内訳を出力
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
-
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
@@ -17,7 +20,6 @@ export function statement(invoice, plays) {
     }
     return result;
   }
-
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of invoice.performances) {
@@ -25,44 +27,43 @@ export function statement(invoice, plays) {
     }
     return result;
   }
-
   function usd(aNumber) {
-    return new Intl.NumberFormat('en-US',
-                        { style: 'currency', currency: 'USD',
-                          minimumFractionDigits: 2 }).format(aNumber/100);
+    return new Intl.NumberFormat('en-US', {
+    style: 'currency', currency: 'USD',
+      minimumFractionDigits: 2
+    }).format(aNumber / 100);
   }
-
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
     // 喜劇のときは10人につき、さらにポイントを加算
-    if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    if ('comedy' === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
     return result;
   }
-
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-
   function amountFor(aPerformance) {
     let result = 0;
     switch (playFor(aPerformance).type) {
-    case 'tragedy':
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case 'comedy':
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+      case 'tragedy':
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
     return result;
   }
 }
+
